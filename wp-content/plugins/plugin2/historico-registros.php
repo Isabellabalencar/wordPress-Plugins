d2d9c68efc7501f25d21af0942ce7fda01c2f876
Isabellabalencar <49618769+Isabellabalencar@users.noreply.github.com>
2023-11-16 14:05:22 -0300

    Update historico-registros.php

--- wp-content/plugins/plugin2/historico-registros.php
+++ wp-content/plugins/plugin2/historico-registros.php
@@ -7,15 +7,7 @@
  */
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-    /**
-     * Imprime um relatório de histórico de registros.
-     *
-     * ## EXEMPLOS
-     *
-     *     wp historico-registros mostrar
-     *
-     * @when after_wp_load
-     */
+  
     function historico_registros_mostrar() {
         // Obtenha as últimas entradas do histórico de registros (substitua isso com sua lógica real).
         $historico_entries = get_last_historico_entries();
@@ -30,16 +22,4 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
     WP_CLI::add_command( 'historico-registros mostrar', 'historico_registros_mostrar' );
 }
 
-/**
- * Função fictícia para obter as últimas entradas do histórico de registros.
- * Substitua isso com sua lógica real para obter os dados do histórico de registros.
- */
-function get_last_historico_entries() {
-    // Implemente a lógica para obter as entradas do histórico de registros.
-    // Aqui, estou apenas retornando algumas entradas fictícias para demonstração.
-    return array(
-        'Entrada 1: Alguma ação realizada em ' . date( 'Y-m-d H:i:s' ),
-        'Entrada 2: Outra ação realizada em ' . date( 'Y-m-d H:i:s' ),
-        'Entrada 3: Mais uma ação realizada em ' . date( 'Y-m-d H:i:s' ),
-    );
-}
+
